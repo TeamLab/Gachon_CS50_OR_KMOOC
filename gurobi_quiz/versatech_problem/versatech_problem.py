29302bd3a9e1e1594896e1ab737cf713ef939b7a
blissray@gmail.com <gachon2014>
2017-07-02 00:37:54 +0900

    Add versatech

---
+++ gurobi_quiz/versatech_problem/versatech_problem.py
@@ -0,0 +1,14 @@
+from gurobipy import *
+
+def modelling():
+    m = Model("versatech_problem")
+    # === Modify codes below using above variable m ===
+
+    return m
+
+if __name__ == "__main__":
+    model_object = modelling()
+    for v in model_object.getVars():
+        print(v.varName, v.x)
+
+    print('Obj:', model_object.objVal)
